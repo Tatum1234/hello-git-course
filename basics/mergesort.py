bb8a075dde78e3bbfb948c4ab44974f717c4688b
Aku Niskanen <aku.niskanen@tuni.fi>
2019-10-21 01:47:04 -0700

    Strings to integers

--- basics/mergesort.py
+++ basics/mergesort.py
@@ -39,14 +39,7 @@ def merge(left, right):
 
 if __name__ == "__main__":
     input_str = input("Enter numbers, separated by ',': ")
-    input_list = input_str.split(",")
-    value_list = []
-    for x in input_list:
-        try:
-            value_list.append(int(x))
-        except ValueError as err:
-            print("Invalid input.")
-            quit(1)
 
-    sorted_list = mergesort(value_list)
-    print(sorted_list)
+    unsorted_list = input_str.split(",")
+
+    debug_print(unsorted_list=unsorted_list)
